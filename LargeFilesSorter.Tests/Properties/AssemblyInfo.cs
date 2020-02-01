69541f968cd173662e7388096abd47cf8bb2647b
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-01 13:45:24 +0200

    .NET Framework version changed

--- LargeFilesSorter.Tests/Properties/AssemblyInfo.cs
+++ LargeFilesSorter.Tests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "LargeFilesSorter.Tests" )]
-[assembly: AssemblyCopyright( "Copyright ©  2018" )]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid( "d6a31e95-f94b-462d-8ec7-793efce43891" )]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
